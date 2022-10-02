65aa93c637ebefca2a43ca0f78c1352bd773a1ab
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-02 14:16:36 +0600

    :construction: second task completed

--- js/task-02.js
+++ js/task-02.js
@@ -2,7 +2,16 @@ const ingredients = [
   'Potatoes',
   'Mushrooms',
   'Garlic',
-  'Tomatos',
+  'Tomatoes',
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+for (const ingredient of ingredients) {
+  const listItem = document.createElement('li')
+  listItem.textContent = ingredient
+  listItem.classList.add('item')
+  ingredientsList.append(listItem)
+}
