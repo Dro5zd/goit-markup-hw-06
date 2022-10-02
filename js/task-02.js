eb93f99c9d4b33cb51dce4390634a8eb72bdd78d
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-02 15:52:05 +0600

    :construction: second task add alternative solve

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,15 @@ const ingredients = [
 
 const ingredientsList = document.querySelector('#ingredients')
 
-for (const ingredient of ingredients) {
-  const listItem = document.createElement('li')
-  listItem.textContent = ingredient
-  listItem.classList.add('item')
-  ingredientsList.append(listItem)
-}
+//// Alternative ////////
+
+// for (const ingredient of ingredients) {
+//   const listItem = document.createElement('li')
+//   listItem.textContent = ingredient
+//   listItem.classList.add('item')
+//   ingredientsList.append(listItem)
+// }
+
+
+const markup = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`).join('')
+ingredientsList.innerHTML = markup
