ce28d0dda253298e4673717445942dee41357521
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-02 13:37:21 +0600

    :construction: first task add alternative solution

--- js/task-01.js
+++ js/task-01.js
@@ -12,3 +12,19 @@ Elements: ${innerList[1].children.length}
 
 Category: ${innerListTitle[2].textContent}
 Elements: ${innerList[2].children.length}`);
+
+/*
+
+////////    Alternative solution     /////////
+
+const listByClass = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${listByClass.length}
+`);
+for (let i of listByClass) {
+ console.log(`
+Category: ${i.firstElementChild.textContent}
+Elements: ${i.lastElementChild.children.length}
+`);
+
+}*/
