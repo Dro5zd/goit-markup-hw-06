0b91f7099359c282851e4353d99f0fb009161273
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-04 11:30:09 +0600

    :bug: 10th task completed

--- js/task-10.js
+++ js/task-10.js
@@ -2,33 +2,37 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const input = document.querySelector('#controls input')
-const boxes = document.querySelector('#boxes')
-const btnCreate = document.querySelector('button[data-create]')
+const input = document.querySelector('#controls input');
+const boxes = document.querySelector('#boxes');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
 
-let value = 0
+let value = 0;
 
-input.addEventListener('input', (event)=>{
-  value = event.currentTarget.value
-  event.currentTarget.reset();
-})
+input.addEventListener('input', (event) => value = event.currentTarget.value);
 
-btnCreate.addEventListener('click', ()=>{
-  createBoxes(value)
-})
+btnCreate.addEventListener('click', () => createBoxes(value));
+
+btnDestroy.addEventListener('click', () => destroyBoxes(value));
 
 
 const createBoxes = (amount) => {
-  const result = []
-for (let i = 0; i < amount; i++) {
-  const li = document.createElement('div')
-  li.style.width = '30px'
-  li.style.height = '30px'
-  li.style.backgroundColor = getRandomHexColor()
-  result.push(li)
-}
-boxes.append(...result)
-}
+  const markup = [];
+  let boxSize = 20;
+  for (let i = 0; i < amount; i++) {
+    boxSize += 10;
+    const li = document.createElement('div');
+    li.style.width = `${boxSize}px`;
+    li.style.height = `${boxSize}px`;
+    li.style.backgroundColor = getRandomHexColor();
+    markup.push(li);
+  }
+  boxes.append(...markup);
+};
+
+const destroyBoxes = () => {
+  boxes.innerHTML = '';
+};
 
 
 
